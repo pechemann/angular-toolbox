eaf893b80a35bca9dfcc0caf3f1640df02278002
Pascal ECHEMANN <pechemann@gmail.com>
2024-06-25 08:43:32 +0200

    add looseReplacer() method test

---
+++ projects/angular-toolbox/src/test/framework/mock/http/path-to-regexp/loose-replacer.spec.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Pascal ECHEMANN. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be found in
+ * the LICENSE file at https://github.com/pechemann/angular-toolbox/blob/main/LICENSE
+ */
+
+import { PLUS } from "../../../../../lib/framework/mock/http/path-to-regexp/constants";
+import { looseReplacer } from "../../../../../lib/framework/mock/http/path-to-regexp/loose-replacer";
+
+describe('looseReplacer', () => {
+  
+  it('should return the escaped string passed in the "value" parameter', () => {
+      expect(looseReplacer("/test")).toEqual("\\/test");
+  });
+
+  it('should return an escaped string with a plus (+) modifier suffix', () => {
+    expect(looseReplacer("/test", "add")).toEqual(`\\/test${PLUS}`);
+});
+});
