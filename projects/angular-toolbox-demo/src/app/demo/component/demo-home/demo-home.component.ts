95e25ef174dce74d765fd4e33728c7f987a940c9
Pascal ECHEMANN <pechemann@gmail.com>
2024-08-10 10:47:34 +0200

    demo page improvment

--- projects/angular-toolbox-demo/src/app/demo/component/demo-home/demo-home.component.ts
+++ projects/angular-toolbox-demo/src/app/demo/component/demo-home/demo-home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AngularToolboxIconListComponent, AngularToolboxPageTitleComponent, BreadcrumbService, IconListItem } from 'projects/angular-toolbox-demo-component-lib/src/public-api';
 import { IconListService } from '../../../ui/model/service/icon-list-list.service';
+import { AngularToolboxLogoComponent } from 'projects/angular-toolbox/src/public-api';
 
 @Component({
   selector: 'app-demo-home',
   standalone: true,
   imports: [
     AngularToolboxIconListComponent,
-    AngularToolboxPageTitleComponent
+    AngularToolboxPageTitleComponent,
+    AngularToolboxLogoComponent
   ],
   templateUrl: './demo-home.component.html',
   styleUrl: './demo-home.component.scss'
