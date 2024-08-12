29940ca65921bb8d542430dfffea31dd4e02206c
Pascal ECHEMANN <pechemann@gmail.com>
2024-08-12 14:16:30 +0200

    finx typo in license headers

--- projects/angular-toolbox/src/lib/model/service/ui/dark-mode.service.ts
+++ projects/angular-toolbox/src/lib/model/service/ui/dark-mode.service.ts
@@ -2,8 +2,8 @@
  * @license
  * Copyright Pascal ECHEMANN. All Rights Reserved.
  *
- * Use of this source code is governed by an MIT-style license that can be ound in
- * fthe LICENSE file at https://pascalechemann.com/angular-toolbox/resources/license
+ * Use of this source code is governed by an MIT-style license that can be found in
+ * the LICENSE file at https://pascalechemann.com/angular-toolbox/resources/license
  */
 
 import { Inject, Injectable, EventEmitter } from '@angular/core';
