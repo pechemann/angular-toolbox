ca91fd46d7512cce73150ec8afc21c512d47c0d3
Pascal ECHEMANN <pechemann@gmail.com>
2024-08-19 10:24:10 +0200

    icon rendering process finalization

--- projects/angular-toolbox/src/lib/framework/mock/http-monitoring-console/component/renderer/icon-renderer/icon-renderer.component.ts
+++ projects/angular-toolbox/src/lib/framework/mock/http-monitoring-console/component/renderer/icon-renderer/icon-renderer.component.ts
@@ -9,6 +9,9 @@
 import { Component, Input } from '@angular/core';
 import { Log } from '../../../../../../model';
 import { ATX_IS_IMPORTED_LOG } from '../../../model/business/atx-is-imported-log';
+import { ConsoleBodyType } from '../../../util/console-body-type';
+import { DataUtil } from '../../../util/data.util';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'atx-icon-renderer',
@@ -19,8 +22,15 @@ import { ATX_IS_IMPORTED_LOG } from '../../../model/business/atx-is-imported-log
 })
 export class AtxIconRendererComponent {
 
+  protected bodyType: ConsoleBodyType = ConsoleBodyType.INVALID;
+  protected hasReponse: boolean = false;
+
   @Input()
   public set data(log: Log) {
+    const response: HttpResponse<any> = log.metadata.response;
+    const complete: boolean = response !== null && response !== undefined;
+    this.hasReponse = complete;
+    if (complete) this.bodyType = DataUtil.getBodyType(response.body);
     this.log = log;
     this.isImported = log.metadata.request?.context.get(ATX_IS_IMPORTED_LOG);
   }
