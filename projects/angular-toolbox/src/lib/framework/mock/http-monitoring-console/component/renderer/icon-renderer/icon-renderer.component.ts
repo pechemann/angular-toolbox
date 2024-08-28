e01291c377d6bda37aa54d34600273b5846fc903
Pascal ECHEMANN <pechemann@gmail.com>
2024-08-28 13:11:45 +0200

    icon renderer refactoring

--- projects/angular-toolbox/src/lib/framework/mock/http-monitoring-console/component/renderer/icon-renderer/icon-renderer.component.ts
+++ projects/angular-toolbox/src/lib/framework/mock/http-monitoring-console/component/renderer/icon-renderer/icon-renderer.component.ts
@@ -13,6 +13,7 @@ import { ConsoleBodyType } from '../../../util/console-body-type';
 import { DataUtil } from '../../../util/data.util';
 import { HttpResponse } from '@angular/common/http';
 import { AtxSpinnerComponent } from '../spinner/spinner.component';
+import { AtxLogRendererBase } from '../../abstract/log-renderer-base';
 
 @Component({
   selector: 'atx-icon-renderer',
@@ -24,21 +25,22 @@ import { AtxSpinnerComponent } from '../spinner/spinner.component';
     AtxSpinnerComponent
   ]
 })
-export class AtxIconRendererComponent {
+export class AtxIconRendererComponent extends AtxLogRendererBase {
 
   protected bodyType: ConsoleBodyType = ConsoleBodyType.INVALID;
   protected hasReponse: boolean = false;
+  protected isImported: boolean = false;
 
   @Input()
-  public set data(log: Log) {
-    const response: HttpResponse<any> = log.metadata.response;
+  public override set log(value: Log | null) {
+    super.log = value;
+    this.hasReponse = this.isImported = false;
+    this.bodyType = ConsoleBodyType.INVALID;
+    if (!value) return;
+    const response: HttpResponse<any> = value.metadata.response;
     const complete: boolean = response !== null && response !== undefined;
     this.hasReponse = complete;
     if (complete) this.bodyType = DataUtil.getBodyType(response.body);
-    this.log = log;
-    this.isImported = log.metadata.request?.context.get(ATX_IS_IMPORTED_LOG);
+    this.isImported = value.metadata.request?.context.get(ATX_IS_IMPORTED_LOG);
   }
-
-  protected isImported: boolean = false;
-  protected log!: Log;
 }
