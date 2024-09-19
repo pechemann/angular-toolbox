45a2fb72bd0576aaba511f10269cdec776305a59
Pascal ECHEMANN <pechemann@gmail.com>
2024-09-19 11:37:02 +0200

    demo menu page improvement

--- projects/angular-toolbox-demo/src/app/demo/component/demo-home/demo-home.component.ts
+++ projects/angular-toolbox-demo/src/app/demo/component/demo-home/demo-home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { AngularToolboxIconListComponent, AngularToolboxPageTitleComponent, BreadcrumbService } from 'projects/angular-toolbox-demo-component-lib/src/public-api';
+import { Component, OnInit } from '@angular/core';
+import { AngularToolboxIconListComponent, AngularToolboxPageTitleComponent, BreadcrumbService, IconListItem } from 'projects/angular-toolbox-demo-component-lib/src/public-api';
 import { IconListService } from '../../../ui/model/service/icon-list-list.service';
 import { AngularToolboxLogoComponent } from 'projects/angular-toolbox/src/public-api';
 
@@ -14,10 +14,19 @@ import { AngularToolboxLogoComponent } from 'projects/angular-toolbox/src/public
   templateUrl: './demo-home.component.html',
   styleUrl: './demo-home.component.scss'
 })
-export class DemoHomeComponent {
+export class DemoHomeComponent implements OnInit {
+  
+  protected itemListCollection: IconListItem[][] = [];
 
-  constructor(public iconListService: IconListService,
+  constructor(private _iconListService: IconListService,
               breadcrumb: BreadcrumbService) {
     breadcrumb.removeAll().addItem(breadcrumb.buildItem("Demo"));
+  }
+
+  public ngOnInit(): void {
+    const itemList: IconListItem[] = this._iconListService.getDemoList();
+    while(itemList.length) {
+      this.itemListCollection.push(itemList.splice(0,10));
+    }
   }
 }
