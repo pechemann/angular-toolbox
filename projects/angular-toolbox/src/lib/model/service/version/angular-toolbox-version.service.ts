367eaf7d4c10541b85d140cd3402a253acb05e20
Pascal ECHEMANN <pechemann@gmail.com>
2024-10-15 07:52:06 +0200

    version update

--- projects/angular-toolbox/src/lib/model/service/version/angular-toolbox-version.service.ts
+++ projects/angular-toolbox/src/lib/model/service/version/angular-toolbox-version.service.ts
@@ -17,8 +17,8 @@ import { VersionConfig } from '../../business';
 const ATX_VERSION_CONFIG: VersionConfig = {
   major: 1,
   minor: 4,
-  patch: 3,
-  buildTimestamp: 1728711207638
+  patch: 4,
+  buildTimestamp: 1728971479807
 };
 
 /**
